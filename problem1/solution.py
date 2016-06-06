025b84abbdaba63bc2d2246ce0752c0f9ac1f316
grehujt <grehujt@users.noreply.github.com>
2016-06-06 23:12:07 +0800

    Update solution.py

--- problem1/solution.py
+++ problem1/solution.py
@@ -5,7 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in xrange(len(nums)):
-            for j in xrange(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        d = {}
+        for i, n1 in enumerate(nums):
+            n2 = target - n1
+            if n2 in d:
+                return [i, d[n2]]
+            else:
+                d[n1] = i
