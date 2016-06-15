414f8355d17bbd54d71f85bad1ddaf3279dd8b9d
grehujt <kidd4212@gmail.com>
2016-06-15 23:27:50 +0800

    update dynamic programming solution

--- 5. Longest Palindromic Substring/solution.py
+++ 5. Longest Palindromic Substring/solution.py
@@ -1,18 +1,3 @@
-## 5. Longest palindromic Substring
-
-- Give a string S, find the longest palindromic substring in S.
-- Maximum length of S is 1000.
-- There exists one unique longest palindromic substring.
-
----
-
-### Analysis
-
-1. Brute-force, iterate all possible substring, O(n^3), 100% TLE
-
-2. Dynamice programming, use a 2d boolean array dp, if dp[i][j] == True, then s[i:j+1] is palindromic. In this case, we have to iterate all possible length of substring as well as starting points, so it is a O(n^2) solution.
-
-```python
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -37,9 +22,5 @@ class Solution(object):
                         l = i
                         r = i+j-1
         return s[l:r+1]
-```
 
-But it still gets TLE...
-
-3. O(n) solution
-> TODO
+print Solution().longestPalindrome('ccc')
