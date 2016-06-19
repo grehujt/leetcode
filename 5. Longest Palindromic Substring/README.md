2d6e397ae70607a0d4f87e67dbf095433da69be4
grehujt <kidd4212@gmail.com>
2016-06-19 18:02:43 +0800

    update O(n) solution

--- 5. Longest Palindromic Substring/README.md
+++ 5. Longest Palindromic Substring/README.md
@@ -42,4 +42,31 @@
     But it still gets TLE...
 
 3. O(n) solution
-> TODO
+    I thought solving this in O(nlog(n)) was the best until I found a O(n), which is called [Manacher’s Algorithm](http://articles.leetcode.com/longest-palindromic-substring-part-ii). My implementation in python:
+
+    ```python
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        newS = '#%s#' % '#'.join(s)
+        length, center, rightMost, maxCenter, maxLen, i = len(newS), 0, 0, 0, 0, 0
+        pArr = [0] * length
+        while i < length:
+            pArr[i] = 1 if rightMost < i else min(rightMost-i, pArr[(center << 1) - i])
+            while i + pArr[i] < length and i - pArr[i] > -1 and newS[i + pArr[i]] == newS[i - pArr[i]]:
+                pArr[i] += 1
+            if i + pArr[i] > rightMost:
+                center = i
+                rightMost = i + pArr[i]
+                if pArr[i] > maxLen:
+                    maxLen = pArr[i]
+                    maxCenter = i
+            i += 1
+        start = (maxCenter - maxLen + 1) >> 1
+        return s[start: start + maxLen - 1]
+    ```
+
+    [some more algorhms](https://leetcode.com/articles/longest-palindromic-substring/).
--- 5. Longest Palindromic Substring/solution.py
+++ 5. Longest Palindromic Substring/solution.py
@@ -4,23 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        length = len(s)
-        if length < 2:
-            return s
-        dp = [[False] * length for ss in s]
-        dp[0][0] = True
-        for i in xrange(1, length):
-            dp[i][i] = True
-            # takes care of length of 2
-            dp[i][i-1] = True
-        l, r = 0, 0
-        for j in xrange(2, length+1):
-            for i in xrange(length-j+1):
-                if s[i] == s[i+j-1] and dp[i+1][i+j-2]:
-                    dp[i][i+j-1] = True
-                    if r-l < j:
-                        l = i
-                        r = i+j-1
-        return s[l:r+1]
+        newS = '#%s#' % '#'.join(s)
+        length, center, rightMost, maxCenter, maxLen, i = len(newS), 0, 0, 0, 0, 0
+        pArr = [0] * length
+        while i < length:
+            pArr[i] = 1 if rightMost < i else min(rightMost-i, pArr[(center << 1) - i])
+            while i + pArr[i] < length and i - pArr[i] > -1 and newS[i + pArr[i]] == newS[i - pArr[i]]:
+                pArr[i] += 1
+            if i + pArr[i] > rightMost:
+                center = i
+                rightMost = i + pArr[i]
+                if pArr[i] > maxLen:
+                    maxLen = pArr[i]
+                    maxCenter = i
+            i += 1
+        start = (maxCenter - maxLen + 1) >> 1
+        return s[start: start + maxLen - 1]
 
-print Solution().longestPalindrome('ccc')
+print Solution().longestPalindrome('cacc')
