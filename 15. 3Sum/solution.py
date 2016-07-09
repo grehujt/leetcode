920c8f227d5753022b805c69e73b6a024064dca4
grehujt <kidd4212@gmail.com>
2016-07-09 15:01:23 +0800

    update solution

--- 15. 3Sum/solution.py
+++ 15. 3Sum/solution.py
@@ -4,3 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        result = []
+        keys = sorted(nums)
+        for i in xrange(len(keys) - 2):
+            if i != 0 and keys[i] == keys[i-1]:  # remove dup
+                continue
+            n1 = keys[i]
+            j, k = i + 1, len(keys) - 1
+            while j < k:
+                n2, n3 = keys[j], keys[k]
+                s = n1 + n2 + n3
+                if s == 0:
+                    result.append([n1, n2, n3])
+                    j += 1
+                    k -= 1
+                    while j < k and keys[j] == keys[j-1]:  # remove dup
+                        j += 1
+                    while j < k and keys[k] == keys[k+1]:  # remove dup
+                        k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return result
