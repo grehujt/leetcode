debe3356679a580c13381d33a1d00d6a0d71d190
grehujt <kidd4212@gmail.com>
2016-07-14 22:35:25 +0800

    update accelerated ver

--- 18. 4Sum/readme.md
+++ 18. 4Sum/readme.md
@@ -21,7 +21,7 @@
 
 ## Solution
 
-- O(n^3):
+- O(n^3) accelerated ver:
 
 ```python
 class Solution(object):
@@ -34,18 +34,34 @@ class Solution(object):
         if len(nums) < 4:
             return []
         nums.sort()
+        if nums[0] << 2 > target or nums[-1] << 2 < target:
+            return []
         result = []
         for i in xrange(len(nums)-3):
             if i != 0 and nums[i] == nums[i-1]:
                 continue
             n1 = nums[i]
+            if n1 + 3*nums[-1] < target:
+                continue
+            if n1 << 2 > target:
+                break
+            if n1 << 2 == target:
+                if n1 == nums[i+3]:
+                    result.append(nums[i:i+4])
+                break
             for j in xrange(i+1, len(nums)-2):
                 if j != i+1 and nums[j] == nums[j-1]:
                     continue
                 n2 = nums[j]
+                if n1 + n2 + (nums[-1] << 1) < target:
+                    continue
+                if n1 + 3*n2 > target:
+                    break
                 l, r = j+1, len(nums)-1
                 while l < r:
                     n3, n4 = nums[l], nums[r]
+                    if n4 << 2 < target or n1+n2+(n3<<1) > target:
+                        break
                     s = n1+n2+n3+n4
                     if s == target:
                         result.append([n1, n2, n3, n4])
--- 18. 4Sum/solution.py
+++ 18. 4Sum/solution.py
@@ -8,34 +8,51 @@ class Solution(object):
         if len(nums) < 4:
             return []
         nums.sort()
-        print nums
+        if nums[0] << 2 > target or nums[-1] << 2 < target:
+            return []
         result = []
-        h, t = 0, len(nums)-1
-        while h < t:
-            n1, n2 = nums[h], nums[t]
-            l, r = h+1, t-1
-            while l < r:
-                n3, n4 = nums[l], nums[r]
-                s = n1+n2+n3+n4
-                if s == target:
-                    result.append([n1, n3, n4, n2])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
-                    while l < r and nums[l] == nums[l-1]:
+        for i in xrange(len(nums)-3):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            n1 = nums[i]
+            if n1 + 3*nums[-1] < target:
+                continue
+            if n1 << 2 > target:
+                break
+            if n1 << 2 == target:
+                if n1 == nums[i+3]:
+                    result.append(nums[i:i+4])
+                break
+            for j in xrange(i+1, len(nums)-2):
+                if j != i+1 and nums[j] == nums[j-1]:
+                    continue
+                n2 = nums[j]
+                if n1 + n2 + (nums[-1] << 1) < target:
+                    continue
+                if n1 + 3*n2 > target:
+                    break
+                l, r = j+1, len(nums)-1
+                while l < r:
+                    n3, n4 = nums[l], nums[r]
+                    if n4 << 2 < target or n1+n2+(n3<<1) > target:
+                        break
+                    s = n1+n2+n3+n4
+                    if s == target:
+                        result.append([n1, n2, n3, n4])
                         l += 1
-                elif s > target:
-                    r -= 1
-                    while l < r and nums[r] == nums[r+1]:
                         r -= 1
-                else:
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]:
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s > target:
+                        r -= 1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
+                    else:
                         l += 1
-            h += 1
-            while h < t and nums[h] == nums[h-1]:
-                h += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
         return result
 
 print Solution().fourSum([1, 0, -1, 0, -2, 2], 0)
