67649ed8687482b9a11bc68273b6209616126ac5
grehujt <kidd4212@gmail.com>
2016-07-15 17:16:09 +0800

    update binary encoding solution

--- 22. Generate Parentheses/readme.md
+++ 22. Generate Parentheses/readme.md
@@ -58,4 +58,30 @@ class Solution(object):
 print Solution().generateParenthesis(10)
 ```
 
+- Binary tree encoding:
 
+```python
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def fill(result, s, l, r):
+            if l == 0 and r == 0:
+                result.append(s)
+                return
+            if l:
+                fill(result, s+'(', l-1, r+1)
+            if r:
+                fill(result, s+')', l, r-1)
+        result = []
+        fill(result, '', n, 0)
+        return result
+```
+
+Take n=2 as an example:
+
+![png](pic1.png)
+
+![png](perf.png)
--- 22. Generate Parentheses/solution.py
+++ 22. Generate Parentheses/solution.py
@@ -4,35 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        def valid(s):
-            stack = []
-            for c in s:
-                if c == '(':
-                    stack.append(c)
-                else:
-                    if len(stack) == 0 or stack.pop() != '(':
-                        return False
-            return len(stack) == 0
-
-        def fill(d):
-            if d == (n << 1):
-                s = ''.join(tmp)
-                if valid(s):
-                    result.append(s)
+        def fill(result, s, l, r):
+            if l == 0 and r == 0:
+                result.append(s)
                 return
-            if tmp[0] == ')':
-                return
-            if d == len(tmp)-1 and tmp[d] == '(':
-                return
-            if tmp[:d].count('(') > n or tmp[:d].count(')') > n:
-                return
-            for c in '()':
-                tmp[d] = c
-                fill(d+1)
-
-        tmp = [''] * (n << 1)
+            if l:
+                fill(result, s+'(', l-1, r+1)
+            if r:
+                fill(result, s+')', l, r-1)
         result = []
-        fill(0)
+        fill(result, '', n, 0)
         return result
 
-print Solution().generateParenthesis(10)
+print Solution().generateParenthesis(3)
