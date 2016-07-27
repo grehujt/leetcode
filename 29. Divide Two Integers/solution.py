24d8f94c8c95aadf8e56381394375a2593471057
grehujt <kidd4212@gmail.com>
2016-07-27 22:50:33 +0800

    update unfinished solution

--- 29. Divide Two Integers/solution.py
+++ 29. Divide Two Integers/solution.py
@@ -5,3 +5,15 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
+        if divisor == 0:
+            return 2147483647
+        isNeg = (dividend<0 and divisor>0) or (dividend>0 and divisor<0)
+        dd, dr, res = abs(dividend), abs(divisor), 0
+        while dr <= dd:
+            tmp, i = dr, 1
+            while tmp <= dd:
+                tmp <<= 1
+                i += 1
+            res += 1 << (i-2)
+            dd -= dr << (i-2)
+        return -res if isNeg else res
