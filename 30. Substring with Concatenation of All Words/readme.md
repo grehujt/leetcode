d0936acfee84e682b42e4262f70ad7d92cb569f9
grehujt <kidd4212@gmail.com>
2016-09-02 18:25:36 +0800

    update refined solution

--- 30. Substring with Concatenation of All Words/readme.md
+++ 30. Substring with Concatenation of All Words/readme.md
@@ -46,3 +46,38 @@ class Solution(object):
                 result.append(i)
         return result
 ```
+
+## refined solution
+```python
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        d1 = {}
+        for w in words:
+            d1[w] = d1[w]+1 if w in d1 else 1
+        singleLen, wordsCnt, result, i = len(words[0]), len(words), [], 0
+        totalLen = singleLen * wordsCnt
+        for i in xrange(min(singleLen, len(s)-totalLen+1)):
+            d2, left, right = {}, i, i
+            while right + singleLen <= len(s):
+                key = s[right : right+singleLen]
+                right += singleLen
+                if key not in d1:
+                    d2 = {}
+                    left = right
+                else:
+                    d2[key] =  d2[key]+1 if key in d2 else 1
+                    while d2[key] > d1[key]:
+                        d2[s[left : left+singleLen]] -= 1
+                        left += singleLen
+                    if right-left == totalLen:
+                        result.append(left)
+        # print result
+        return result
+```
+
+![png](perf.png) 
--- 30. Substring with Concatenation of All Words/solution.py
+++ 30. Substring with Concatenation of All Words/solution.py
@@ -1,4 +1,4 @@
-# from collections import Counter  ## get TLE!!!
+# from collections import Counter  ## slow!!!
 class Solution(object):
     def findSubstring(self, s, words):
         """
@@ -8,28 +8,27 @@ class Solution(object):
         """
         d1 = {}
         for w in words:
-            if w not in d1:
-                d1[w] = 1
-            else:
-                d1[w] += 1
-        singleLen, wordsCnt = len(words[0]), len(words)
-        result = []
-        for i in xrange(len(s) - singleLen * wordsCnt + 1):
-            d2 = {}
-            j = 0
-            while j < wordsCnt:
-                key = s[i+j*singleLen:i+(j+1)*singleLen]
+            d1[w] = d1[w]+1 if w in d1 else 1
+        singleLen, wordsCnt, result, i = len(words[0]), len(words), [], 0
+        totalLen = singleLen * wordsCnt
+        for i in xrange(min(singleLen, len(s)-totalLen+1)):
+            d2, left, right = {}, i, i
+            while right + singleLen <= len(s):
+                key = s[right : right+singleLen]
+                right += singleLen
                 if key not in d1:
-                    break
-                if key not in d2:
-                    d2[key] = 1
+                    d2 = {}
+                    left = right
                 else:
-                    d2[key] += 1
-                    if d2[key] > d1[key]:
-                        break
-                j += 1
-            if j == wordsCnt:
-                result.append(i)
+                    d2[key] =  d2[key]+1 if key in d2 else 1
+                    while d2[key] > d1[key]:
+                        d2[s[left : left+singleLen]] -= 1
+                        left += singleLen
+                    if right-left == totalLen:
+                        result.append(left)
+        # print result
         return result
 Solution().findSubstring('abcba',['a','b'])
+# Solution().findSubstring("barfoothefoobarman",["foo","bar"])
+Solution().findSubstring("ababaab",["ab","ba","ba"])
 
