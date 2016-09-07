ede63d098ece1c036e538335b31a229da98b6921
grehujt <kidd4212@gmail.com>
2016-09-07 23:41:39 +0800

    try stack approach

--- 32. Longest Valid Parentheses/solution.py
+++ 32. Longest Valid Parentheses/solution.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        stack = []
+        cnt = 0
+        maxCnt = 0
+        for c in s:
+            if c == '(':
+                stack.append(c)
+            else:
+                if stack:
+                    stack.pop()
+                    cnt += 2
+                else:
+                    cnt = 0
+                    stack = []
+                maxCnt = max(maxCnt, cnt)
+        return maxCnt
+
+print Solution().longestValidParentheses('((())')
 
