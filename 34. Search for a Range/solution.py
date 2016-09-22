2a99f50f4bad76afbd34c208b1d4cca05092ffde
grehujt <kidd4212@gmail.com>
2016-09-22 23:10:45 +0800

    update divide & conquer solution

--- 34. Search for a Range/solution.py
+++ 34. Search for a Range/solution.py
@@ -1,6 +1,3 @@
-import bisect
-
-
 class Solution(object):
     def searchRange(self, nums, target):
         """
@@ -8,10 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        lo = bisect.bisect_left(nums, target)
-        if lo==len(nums) or nums[lo]!=target:
+        def _search(lo, hi):
+            if nums[lo] == target == nums[hi]:
+                return [lo, hi]
+            if nums[lo] <= target <= nums[hi]:
+                mid = lo + ((hi-lo) >> 1)
+                l, h = _search(lo, mid), _search(mid+1, hi)
+                return max(l, h) if -1 in l+h else [l[0], h[1]]
             return [-1, -1]
-        else:
-            return [lo, bisect.bisect(nums, target, lo=lo+1)-1]
+        return _search(0, len(nums)-1)
 
 print Solution().searchRange([1,2,2,3], 2)
