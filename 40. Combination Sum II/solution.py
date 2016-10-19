b0e58762cb8ac891948a4735d5f7b299df6f76a1
grehujt <kidd4212@gmail.com>
2016-10-19 20:34:54 +0800

    refine solution

--- 40. Combination Sum II/solution.py
+++ 40. Combination Sum II/solution.py
@@ -5,26 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        def bt(beg, t, tmp, solSet):
+        def bt(beg, t, tmp):
             if t == 0:
-                sol = ''.join(str(x) for x in tmp)
-                if sol not in solSet:
-                    solSet.add(sol)
-                    result.append([x for x in tmp])
+                result.append([x for x in tmp])
             else:
                 for i in xrange(beg, len(candidates)):
                     d = candidates[i]
+                    if i!=beg and d==candidates[i-1]:
+                        continue
                     tmp.append(d)
                     if t-d < 0:
                         tmp.pop()
                         return
                     else:
-                        bt(i+1, t-d, tmp, solSet)
+                        bt(i+1, t-d, tmp)
                         tmp.pop()
 
         result = []
         candidates.sort()
-        bt(0, target, [], set())
+        bt(0, target, [])
         return result
 
 print Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
