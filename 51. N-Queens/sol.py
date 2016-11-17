d541b0171f4fd5129178cb20a043bf825850c6e8
grehujt <kidd4212@gmail.com>
2016-11-17 23:43:48 +0800

    update solution

--- 51. N-Queens/sol.py
+++ 51. N-Queens/sol.py
@@ -1,36 +1,3 @@
-# 51. N-Queens
-
-## Problem
-- The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
-- Given an integer n, return all distinct solutions to the n-queens puzzle.
-- Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
-
-> For example,
-> 
-> There exist two distinct solutions to the 4-queens puzzle:
-> 
-> [
-> 
->  [".Q..",  // Solution 1
->  
->   "...Q",
->   
->   "Q...",
->   
->   "..Q."],
-> 
->  ["..Q.",  // Solution 2
->  
->   "Q...",
->   
->   "...Q",
->   
->   ".Q.."]
->   
-> ]
-
-## Solution
-```python
 class Solution(object):
     def solveNQueens(self, n):
         """
@@ -53,6 +20,5 @@ class Solution(object):
         const = (n << 1) - 1
         bt(boards, [['.']*n for _ in range(n)], 0, [False]*n, [False]*const, [False]*const)
         return boards
-```
 
-![pic](pic.png)
+Solution().solveNQueens(4)
