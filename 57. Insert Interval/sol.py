dd206c1beb9bbdf2a40729b5605f5a8de673080d
grehujt <kidd4212@gmail.com>
2016-12-04 17:35:52 +0800

    update sol

--- 57. Insert Interval/sol.py
+++ 57. Insert Interval/sol.py
@@ -1,26 +1,12 @@
-# 57. Insert Interval
+import bisect
 
-## Problem
-- Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
-- You may assume that the intervals were initially sorted according to their start times.
 
-> Example 1:
-> 
-> Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
-> 
-> Example 2:
-> 
-> Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
-> 
-> This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
-
-## Solution
-```python
 # Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
 
 class Solution(object):
     def insert(self, intervals, newInterval):
@@ -42,4 +28,10 @@ class Solution(object):
             else:
                 ret.append(item2)
         return ret
-```
+
+
+# intervals = [Interval(0, 2), Interval(1, 3), Interval(4, 5)]
+intervals = [Interval(1, 5)]
+newInterval = Interval(1, 7)
+for item in Solution().insert(intervals, newInterval):
+    print item.start, item.end
