a7b4c20f5e6ad3346f6f95cb0a6c1e4cafbfebd2
grehujt <kidd4212@gmail.com>
2016-12-29 20:14:07 +0800

    update sol

--- 68. Text Justification/sol.py
+++ 68. Text Justification/sol.py
@@ -1,31 +1,3 @@
-# 68. Text Justification
-
-## Problem
-- Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
-- You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly L characters.
-- Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
-- For the last line of text, it should be left justified and no extra space is inserted between words.
-
-> For example,
-> 
-> words: ["This", "is", "an", "example", "of", "text", "justification."]
-> 
-> L: 16.
-> 
-> Return the formatted lines as:
-> 
-> [
-> 
->    "This    is    an",
->    
->    "example  of text",
->    
->    "justification.  "
->    
-> ]
-
-## Solution
-```python
 class Solution(object):
     def fullJustify(self, words, maxWidth):
         """
@@ -61,4 +33,7 @@ class Solution(object):
         if row:
             ret.append(process_row(row, rowLen, isLast=True))
         return ret
-```
+
+words = ["This", "is", "an", "example", "of", "text", "justification."]
+for r in Solution().fullJustify(words, 16):
+    print r
