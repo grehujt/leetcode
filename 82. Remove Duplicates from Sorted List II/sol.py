8a3223d2e8a942c3a47a5aa65221cd3c6961e5d5
grehujt <kidd4212@gmail.com>
2017-02-12 14:44:20 +0800

    refine sol

--- 82. Remove Duplicates from Sorted List II/sol.py
+++ 82. Remove Duplicates from Sorted List II/sol.py
@@ -1,21 +1,8 @@
-# 82. Remove Duplicates from Sorted List II
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-## Problem
-- Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
-
-> For example,
-> 
-> Given 1->2->3->3->4->4->5, return 1->2->5.
-> 
-> Given 1->1->1->2->3, return 2->3.
-
-## Solution
-```python
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 
 class Solution(object):
     def deleteDuplicates(self, head):
@@ -35,4 +22,3 @@ class Solution(object):
             cur = cur.next
             pre.next = None
         return dummy.next
-```
